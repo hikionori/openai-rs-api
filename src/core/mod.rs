29be5a3506c2436de3ec3917bf8679827551f387
Danila Verbinskiy <work.danv4656@gmail.com>
2023-07-04 08:16:54 +0300

    Add documentation

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -10,8 +10,8 @@ use self::models::{
     chat::{ChatParameters, ChatResponse},
     completions::{CompletionParameters, CompletionResponse},
     edits::{EditParameters, EditResponse},
+    images::{ImageCreateParameters, ImageEditParameters, ImageResponse, ImageVariationParameters},
     list_models::{Model, ModelList},
-    images::{ImageCreateParameters, ImageResponse, ImageEditParameters, ImageVariationParameters},
 };
 
 pub struct OpenAI {
@@ -23,17 +23,17 @@ pub struct OpenAI {
 impl OpenAI {
     /// The function creates a new instance of the OpenAI struct with the provided token and
     /// organization, along with an HTTPS client.
-    /// 
+    ///
     /// Arguments:
-    /// 
+    ///
     /// * `token`: The `token` parameter is a string that represents the authentication token used to
     /// access the OpenAI API. This token is typically provided by OpenAI when you sign up for their
     /// services.
     /// * `oia_org`: The `oia_org` parameter represents the OpenAI organization ID. It is used to
     /// identify the organization associated with the API token being used.
-    /// 
+    ///
     /// Returns:
-    /// 
+    ///
     /// The `new` function returns an instance of the `OpenAI` struct.
     pub fn new(token: String, oia_org: String) -> Self {
         let https_client = Client::builder()
@@ -64,9 +64,9 @@ impl OpenAI {
 
     /// The function `list_models` sends a GET request to the OpenAI API to retrieve a list of models
     /// and returns the parsed response as a `ModelList` object.
-    /// 
+    ///
     /// Returns:
-    /// 
+    ///
     /// a Result object with the type ModelList.
     pub async fn list_models(self) -> Result<ModelList, Box<dyn Error>> {
         let client = self.https_client;
@@ -87,14 +87,14 @@ impl OpenAI {
 
     /// The function retrieves a model from the OpenAI API using an HTTPS client and returns the parsed
     /// model response.
-    /// 
+    ///
     /// Arguments:
-    /// 
+    ///
     /// * `model`: The `model` parameter is a `String` that represents the name of the model you want to
     /// retrieve. It is used to construct the URL for the API request.
-    /// 
+    ///
     /// Returns:
-    /// 
+    ///
     /// a Result object with the type Model as the Ok variant and Box<dyn Error> as the Err variant.
     pub async fn retrive_model(self, model: String) -> Result<Model, Box<dyn Error>> {
         let client = self.https_client;
@@ -113,18 +113,17 @@ impl OpenAI {
         Ok(serde_json::from_slice::<Model>(&result).expect("Failed to parse model response"))
     }
 
-    
     /// The function `create_chat_completions` sends a POST request to the OpenAI API to generate chat completions
     /// based on the given parameters.
-    /// 
+    ///
     /// Arguments:
-    /// 
+    ///
     /// * `parameters`: The `parameters` parameter in the `create_chat_completions` function is of type
     /// `ChatParameters`. It is an input parameter that contains the information required to
     /// generate chat completions using the OpenAI API.
-    /// 
+    ///
     /// Returns:
-    /// 
+    ///
     /// a `Result` with a `ChatResponse` on success or a `Box<dyn Error>` on failure.
     pub async fn create_chat_completions(
         self,
@@ -149,15 +148,15 @@ impl OpenAI {
 
     /// The function `create_completions` sends a POST request to the OpenAI API to generate completions
     /// based on the given parameters.
-    /// 
+    ///
     /// Arguments:
-    /// 
+    ///
     /// * `parameters`: The `parameters` parameter in the `create_completions` function is of type
     /// `CompletionParameters`. It is an input parameter that contains the information required to
     /// generate completions using the OpenAI API.
-    /// 
+    ///
     /// Returns:
-    /// 
+    ///
     /// a `Result` with a `CompletionResponse` on success or a `Box<dyn Error>` on failure.
     pub async fn create_completions(
         self,
@@ -183,16 +182,16 @@ impl OpenAI {
 
     /// The function `create_edit` sends a POST request to the OpenAI API to create or edit a completion
     /// and returns the response.
-    /// 
+    ///
     /// Arguments:
-    /// 
+    ///
     /// * `parameters`: The `parameters` parameter in the `create_edit` function is of type
     /// `EditParameters`. It is an input parameter that contains the necessary information for creating
     /// an edit. The specific fields and their meanings depend on the implementation of the
     /// `EditParameters` struct. You would need to refer to the definition
-    /// 
+    ///
     /// Returns:
-    /// 
+    ///
     /// a `Result` with the type `EditResponse` on success or a `Box<dyn Error>` on failure.
     pub async fn create_edit(
         self,
@@ -215,8 +214,23 @@ impl OpenAI {
         Ok(serde_json::from_slice::<EditResponse>(&result).expect("Failed to parse edit response"))
     }
 
-
-    pub async fn create_image(self, parameters: ImageCreateParameters) -> Result<ImageResponse, Box<dyn Error>> {
+    /// The `create_image` function sends a POST request to the OpenAI API to generate an image based on
+    /// the provided parameters.
+    ///
+    /// Arguments:
+    ///
+    /// * `parameters`: The `parameters` parameter in the `create_image` function is of type
+    /// `ImageCreateParameters`. It is an input parameter that contains the necessary information for
+    /// generating an image.
+    ///
+    /// Returns:
+    ///
+    /// The function `create_image` returns a `Result` enum with the success case containing an
+    /// `ImageResponse` and the error case containing a `Box<dyn Error>`.
+    pub async fn create_image(
+        self,
+        parameters: ImageCreateParameters,
+    ) -> Result<ImageResponse, Box<dyn Error>> {
         let client = self.https_client;
         let url = String::from("https://api.openai.com/v1/images/generations");
 
@@ -231,10 +245,27 @@ impl OpenAI {
             .await
             .unwrap();
 
-        Ok(serde_json::from_slice::<ImageResponse>(&result).expect("Failed to parse image response"))
+        Ok(serde_json::from_slice::<ImageResponse>(&result)
+            .expect("Failed to parse image response"))
     }
 
-    pub async fn create_image_edit(self, parameters: ImageEditParameters) -> Result<ImageResponse, Box<dyn Error>> {
+    /// The function `create_image_edit` sends a POST request to the OpenAI API to create an image edit,
+    /// using the provided parameters, and returns the resulting image response.
+    ///
+    /// Arguments:
+    ///
+    /// * `parameters`: The `parameters` parameter in the `create_image_edit` function is of type
+    /// `ImageEditParameters`. It is an input parameter that contains the necessary information for
+    /// creating an image edit.
+    ///
+    /// Returns:
+    ///
+    /// a Result type with the success variant containing an ImageResponse or the error variant
+    /// containing a Box<dyn Error>.
+    pub async fn create_image_edit(
+        self,
+        parameters: ImageEditParameters,
+    ) -> Result<ImageResponse, Box<dyn Error>> {
         let client = self.https_client;
         let url = String::from("https://api.openai.com/v1/images/edits");
 
@@ -249,10 +280,26 @@ impl OpenAI {
             .await
             .unwrap();
 
-        Ok(serde_json::from_slice::<ImageResponse>(&result).expect("Failed to parse image response"))
+        Ok(serde_json::from_slice::<ImageResponse>(&result)
+            .expect("Failed to parse image response"))
     }
 
-    pub async fn create_image_variations(self, parameters: ImageVariationParameters) -> Result<ImageResponse, Box<dyn Error>> {
+    /// The function `create_image_variations` sends a POST request to the OpenAI API to create image
+    /// variations based on the provided parameters.
+    ///
+    /// Arguments:
+    ///
+    /// * `parameters`: The `parameters` parameter in the `create_image_variations` function is of type
+    /// `ImageVariationParameters`. It is an input parameter that contains the necessary information for
+    /// creating image variations.
+    ///
+    /// Returns:
+    ///
+    /// a Result object with the type ImageResponse.
+    pub async fn create_image_variations(
+        self,
+        parameters: ImageVariationParameters,
+    ) -> Result<ImageResponse, Box<dyn Error>> {
         let client = self.https_client;
         let url = String::from("https://api.openai.com/v1/images/variations");
 
@@ -267,7 +314,7 @@ impl OpenAI {
             .await
             .unwrap();
 
-        Ok(serde_json::from_slice::<ImageResponse>(&result).expect("Failed to parse image response"))
+        Ok(serde_json::from_slice::<ImageResponse>(&result)
+            .expect("Failed to parse image response"))
     }
-
 }
